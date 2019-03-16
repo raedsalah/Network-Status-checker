99a52d10d9962123bfb4b6301b65d383cb6738d7
Raed Salah <dkraed@gmail.com>
2019-03-16 11:52:18 +0300

    added comments

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+  //gets the network status from the browser navigator api once page is loaded
   if (navigator.onLine) {
     console.log("online");
     $("#content").html("online <span class='dot online'></span>");
@@ -6,6 +7,8 @@ $(document).ready(function() {
     console.log("offline");
     $("#content").html("offline <span class='dot offline'></span>");
   }
+
+  //event listener for changes in the netwrok
   window.addEventListener("offline", function(e) {
     console.log("offline");
     $("#content").html("offline <span class='dot offline'></span>");

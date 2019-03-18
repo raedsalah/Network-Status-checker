8a50ed001979bd3f2a427128021db75b8438222f
Raed Salah <dkraed@gmail.com>
2019-03-18 15:45:10 +0300

    now displaying ip and network status

--- main.js
+++ main.js
@@ -2,22 +2,49 @@ $(document).ready(function() {
   //gets the network status from the browser navigator api once page is loaded
   if (navigator.onLine) {
     console.log("online");
-    $("#netchk").removeClass("offline");
-    $("#netchk").addClass("online");
+    $("#netchk").html("online");
+    $(".dot").removeClass("offline");
+    $(".dot").addClass("online");
   } else {
     console.log("offline");
-    $("#netchk").removeClass("online");
-    $("#netchk").addClass("offline");
+    $("#netchk").html("offline");
+    $(".dot").removeClass("online");
+    $(".dot").addClass("offline");
   }
 
   //event listener for changes in the netwrok
   window.addEventListener("offline", function(e) {
-    $("#netchk").removeClass("online");
-    $("#netchk").addClass("offline");
+    $("#netchk").html("offline");
+    $(".dot").removeClass("online");
+    $(".dot").addClass("offline");
   });
 
   window.addEventListener("online", function(e) {
-    $("#netchk").removeClass("offline");
-    $("#netchk").addClass("online");
+    console.log("online");
+    $("#netchk").html("online");
+    $(".dot").removeClass("offline");
+    $(".dot").addClass("online");
   });
 });
+
+var findIP = new Promise(r => {
+  var w = window,
+    a = new (w.RTCPeerConnection ||
+      w.mozRTCPeerConnection ||
+      w.webkitRTCPeerConnection)({ iceServers: [] }),
+    b = () => {};
+  a.createDataChannel("");
+  a.createOffer(c => a.setLocalDescription(c, b, b), b);
+  a.onicecandidate = c => {
+    try {
+      c.candidate.candidate
+        .match(
+          /([0-9]{1,3}(\.[0-9]{1,3}){3}|[a-f0-9]{1,4}(:[a-f0-9]{1,4}){7})/g
+        )
+        .forEach(r);
+    } catch (e) {}
+  };
+});
+findIP
+  .then(ip => $("#ipchk").html("your ip: " + ip))
+  .catch(e => console.error(e));
